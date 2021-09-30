61877234d2d11e81419e01d41f0780090e38bcd6
Bryce Blankinship <blankinshipbusiness@gmail.com>
2021-09-30 16:48:45 -0400

    Get main page established

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,7 +7,7 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('scroll', event => {
 
     // Navbar shrink function
     var navbarShrink = function () {
